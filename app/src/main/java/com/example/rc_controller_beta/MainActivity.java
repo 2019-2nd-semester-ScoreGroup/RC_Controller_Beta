e480911d4bb2c5e36d77340d296193c56f3b289c
JUMINJONG <wnalsals12@naver.com>
2020-06-14 13:56:49 +0900

    앱 로고, 런처 아이콘 수정

--- app/src/main/java/com/example/rc_controller_beta/MainActivity.java
+++ app/src/main/java/com/example/rc_controller_beta/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.example.rc_controller_beta.helpers.CameraPermissionHelper;
@@ -35,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         if (!checkIsSupportedDeviceOrFinish(this)) {
